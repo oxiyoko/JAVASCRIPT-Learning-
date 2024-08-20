a4692d481810eb6cbb29a4c7319b93b8aed6f01e
ABHIJITH T <HI.OXIYOKO@GMAIL.COM>
2024-08-20 14:06:05 +0000

    variable

---
+++ 04_variables.js
@@ -0,0 +1,13 @@
+
+const accountId = 30;
+let accountEmail = "oxiyoko@google.com"
+var accountPassword = "12345"
+accountCity = "Kerla"
+
+/*
+
+const  -> you cant change the value . Its constant value
+accountid = 2; you cant change because of its a constant variable
+
+let ->
+*/
