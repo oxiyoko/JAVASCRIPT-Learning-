ff811bc96852757a6b7ec2adf1a242db76b3a9e7
ABHIJITH T <HI.OXIYOKO@GMAIL.COM>
2024-08-21 14:18:27 +0000

    06 lec

---
+++ 06_conversion.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+let valueInNumber = Number(score) // converted into number
+
+/*
+
+let scoreNumber = "56abc" 
+let valueScoreNumber = Number(scoreNumber);  
+console.log(typeof valueScoreNumber);  // output : number
+console.log(valueScoreNumber);  // output : Nan
+
+
+<>  Here we are trying to convert a string to a number and its giving us input Nan as value
+<>  Nan --> Not a number
+
+
+<>  "33"       --> 33
+<>  "33abc"    --> 
+<>  true  -->  1
+<>  false -->  0
+
+*/
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = 
