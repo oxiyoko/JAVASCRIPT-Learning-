3486e519e2fe0d5479e055a81706f8e6cce94861
ABHIJITH T <HI.OXIYOKO@GMAIL.COM>
2024-08-21 13:03:26 +0000

    finished 4th Lecture and note

--- 04_variables.js
+++ 04_variables.js
@@ -9,5 +9,17 @@ accountCity = "Kerla"
 const  -> you cant change the value . Its constant value
 accountid = 2; you cant change because of its a constant variable
 
-let ->
+Prefer not to use var
+because of issue in block scope and functional scope
+
+
+in javascript we can also declare variable not giving datatypes
+
+*/
+
+console.table([accountId, accountEmail, accountPassword])
+
+/*
+
+
 */
