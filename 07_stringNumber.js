b9736861ff4d5533c61191b350aaffbcbc1980de
ABHIJITH T <HI.OXIYOKO@GMAIL.COM>
2024-08-22 14:50:03 +0000

    string and number conversions

---
+++ 07_stringNumber.js
@@ -0,0 +1,51 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+
+
+// console.log(5 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(5 / 2);
+// console.log(5 % 2);
+
+
+let str1 = "hello";
+let str2 = " oxiyoko";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("5" + 2 + 2);
+// console.log(5 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+// Dont try to use tricky behaviour
+// code readability should be high
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+
+// prefix and postfix
+let prefix = 100;
+let postfix = 100;
+let valuePrefix =  ++prefix;
+let valuePostfix = postfix++;
+console.log("prefix",prefix);
+console.log("postfix", postfix);
+console.log("valuePrefix", valuePrefix);
+console.log("valuePostfix: ", valuePostfix);
+
+
+
+
