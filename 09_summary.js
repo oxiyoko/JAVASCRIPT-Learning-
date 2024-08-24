9432fc090a2d0b09748aa7f8189c9e46bff08aa1
ABHIJITH T <HI.OXIYOKO@GMAIL.COM>
2024-08-24 12:35:44 +0000

    summary

---
+++ 09_summary.js
@@ -0,0 +1,24 @@
+/*
+
+Primitive datatype 
+
+1 . String
+2 . Number
+3 . Boolean
+4 . Null
+5 . Undefined
+6 . Symbol
+7 . BigInt 
+
+
+Non primitve datatype - Reference datatype
+
+1 . Array
+2 . Object
+3 . Function
+ 
+*/
+
+// const score = 100;
+// const score = false;
+// const scoreValue = 100.3;    
