c6ceea373692707770ccd3d4ef7ed82373ce0f3b
ABHIJITH T <HI.OXIYOKO@GMAIL.COM>
2024-08-25 13:19:38 +0000

    primitive and non primitive

--- 09_summary.js
+++ 09_summary.js
@@ -22,3 +22,45 @@ Non primitve datatype - Reference datatype
 // const score = 100;
 // const score = false;
 // const scoreValue = 100.3;    
+// const isLoggedIn = false;
+// const outSide = null;
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 34234324234342423423242423432423n;
+// console.log(typeof bigNumber)
+
+// to convert to bigInt write n at the end of the number like this 342342n
+
+const heros = ['iron man', 'spider man', 'loki'];
+let myObj = {
+    name : 'oxiyoko',
+    age:21,
+}
+
+
+const myFunction = function(){
+    console.log("Hello oxiyoko");
+}
+
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+
+/*
+
+typeof 
+undefined -> undefined
+null -> Object
+Boolean -> Boolean
+Number -> Number
+String -> String
+
+*/
+
+
+
